c83808c94c71c9f9f33ada9868a94efefc8da94a
Vitor <vitormpb1996@gmail.com>
2021-05-26 23:03:52 +0100

    Add contacts and new map image

--- js/index.js
+++ js/index.js
@@ -1,26 +1,2 @@
-function myMap() {
-    return;
-    var mapProp= {
-      center:new google.maps.LatLng(38.800049679829435,-7.45592387412418),
-      zoom:5,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
 
-
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat: 38.800049679829435, lng: -7.45592387412418 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("googleMap"), {
-      zoom: 4,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-  }
   
